c335c15cadc3abcdbb6800308d31f0c70a6989ea
sjbarag <sjbarag@gmail.com>
2011-11-05 10:24:04 -0400

    Add socket implementation
    Blocked here until I get a barebones server running

--- src/com/designatum_1393/wejay/WeJay.java
+++ src/com/designatum_1393/wejay/WeJay.java
@@ -10,6 +10,15 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.content.Context;
 
+// for socket
+import java.net.Socket;
+import java.net.InetAddress;
+import android.util.Log;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 public class WeJay extends Activity
 {
     /** Called when the activity is first created. */
@@ -24,6 +33,26 @@ public class WeJay extends Activity
 	{
 		Context ctx = getApplicationContext();
 		Toast tst = Toast.makeText(ctx, "WOOOOAHHH WOAH WOOOOOAHHH WOAH", Toast.LENGTH_SHORT);
+
+		/** create socket **/
+		Socket skt;
+		try {
+			skt = new Socket( "10.0.0.9", 80 );
+
+			// Write to socket source: http://thinkandroid.wordpress.com/2010/03/27/incorporating-socket-programming-into-your-applications/
+			PrintWriter out;
+			try {
+				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(skt.getOutputStream())), true);
+				out.println("I <3 Tom Jones.");
+			} catch (Exception e) {
+				Log.e("WeJay", "Could not write to socket!");
+			}
+		}
+		catch (IOException e) {
+			Log.e("WeJay", "Could not create socket!");
+			Log.e("WeJay", ""+e);
+		}
+
 		tst.show();
 	}
 }
